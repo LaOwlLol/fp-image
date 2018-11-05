bbe879f7acd7dfd056f8bc1d9419a84a78e253c4
LaOwlLol <something.less.than.nate@gmail.com>
2018-11-05 00:23:50 -0600

    deleted deprecated Convolution interface.

--- src/main/java/fauxpas/filters/Convolution.java
+++
@@ -1,9 +0,0 @@
-package fauxpas.filters;
-
-import javafx.scene.image.Image;
-import javafx.scene.image.PixelReader;
-
-public interface Convolution {
-
-    double[][][] computeKernel(Image target, PixelReader targetReader, double[][] convolution, int imageX, int imageY);
-}
