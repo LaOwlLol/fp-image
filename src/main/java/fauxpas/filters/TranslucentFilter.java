c60855d793d5f109d1c77915e2e451d3b71b0d5d
LaOwlLol <something.less.than.nate@gmail.com>
2019-02-25 18:25:22 -0800

    Added TranslucentFilter which calculates gray scale value of pixels and sets the opacity to the gray value.

---
+++ src/main/java/fauxpas/filters/TranslucentFilter.java
@@ -0,0 +1,38 @@
+package fauxpas.filters;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+
+public class TranslucentFilter implements Filter {
+
+    @Override
+    public Image apply(Image image) {
+
+        WritableImage buffer = new WritableImage((int)image.getWidth(), (int)image.getHeight());
+        PixelWriter bufferWriter = buffer.getPixelWriter();
+
+        PixelReader imageReader = image.getPixelReader();
+
+        for (int j = 0; j < image.getHeight(); ++j) {
+            for (int i = 0; i < image.getWidth(); ++i) {
+                Color imageColor = imageReader.getColor(i, j);
+
+                double alpha = Math.min(1.0, (0.3*imageColor.getRed()) +
+                        (0.59 * imageColor.getGreen()) +
+                        (0.11 * imageColor.getBlue()));
+
+                bufferWriter.setColor(i, j, new Color(
+                        imageColor.getRed(),
+                        imageColor.getGreen(),
+                        imageColor.getBlue(),
+                        alpha));
+            }
+        }
+
+        return buffer;
+    }
+
+}
